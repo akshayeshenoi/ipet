c30f240f392fdf57b4b6f8a7e5341625cff7fe61
Akshaye Shenoi <akshaye.shenoi@gmail.com>
2022-11-29 17:24:47 +0800

    added alternate models

--- IoT_Device_Fingerprinting/Train_Mseq.py
+++ IoT_Device_Fingerprinting/Train_Mseq.py
@@ -3,11 +3,13 @@ import os
 from sklearn.preprocessing import OneHotEncoder
 import tensorflow as tf
 from tensorflow.keras import backend as K
-from tensorflow.keras.layers import Dense, LSTM, Bidirectional, Concatenate, Input, Dropout
+from tensorflow.keras.layers import Dense, LSTM, Bidirectional, Concatenate, Input, Dropout,Conv1D, GRU, Concatenate,MaxPooling1D,Input, Dropout,Flatten
 from tensorflow.keras.models import Model
 from tensorflow.keras.regularizers import l2
 from tensorflow.keras.optimizers import Adam, SGD
 from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
+from constants import train_sit
+
 
 def custom_split(features_main,silences,labels):
     idx_new = np.random.permutation(features_main.shape[0])
@@ -25,27 +27,113 @@ def custom_split(features_main,silences,labels):
 
     return X_train_feat,X_test_feat,X_train_silence,X_test_silence,Y_train,Y_test
 
-def get_model():
+def LSTM_discriminator(x_sample,y_sample):
     dtype = 'float64'
     dropout=0.2
 
-    input_data_1 = Input(name='main_Features', shape=X_train_feat[0].shape, dtype=dtype)
-    input_data_2 = Input(name='silence', shape=(1), dtype=dtype)
+    input_data_1 = Input(name='Main_Features', shape = x_sample[1:], dtype=dtype)
+    input_data_2 = Input(name='Silence', shape=(1), dtype=dtype)
 
-    att_in = Bidirectional(LSTM(64,return_sequences=True,kernel_regularizer=l2(),name='lstm1'))(input_data_1)
-    att_out = Bidirectional(LSTM(64,return_sequences=False,name='lstm2'))(att_in)
-
-
+    att_in = Bidirectional(LSTM(8,return_sequences=True,kernel_regularizer=l2()))(input_data_1)
+    att_out = Bidirectional(LSTM(8,return_sequences=False))(att_in)
+    
     concatted = Concatenate()([att_out,input_data_2])
     x = Dense(units=128, activation='tanh', name='fc')(concatted)
     x = Dropout(dropout, name='dropout_2')(x)
 
-    y_pred = Dense(units=Y_test.shape[1], activation='softmax', name='softmax')(x) 
+    # Output layer with softmax
+    y_pred = Dense(units=y_sample, activation='softmax', name='softmax')(x) 
 
     K.clear_session()
     model = Model(inputs=[input_data_1,input_data_2], outputs=y_pred)
     model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
     return model 
+
+def CNN_discriminator(x_sample,y_sample):
+    dtype = 'float64'
+    dropout=0.2
+    
+    n_timesteps, n_features = x_sample[1], x_sample[2]
+
+    input_data_1 = Input(name='Main_Features', shape = (x_sample[1],x_sample[2]), dtype=dtype)
+    input_data_2 = Input(name='Silence', shape=(1), dtype=dtype)
+
+    att_in = Conv1D(filters=64, kernel_size=3, activation='relu', input_shape=(n_timesteps,n_features))(input_data_1)
+    att_out = Conv1D(filters=64, kernel_size=3, activation='relu')(att_in)
+    x = Dropout(0.5)(att_out)
+    x = MaxPooling1D(pool_size=2)(x)
+    x = Flatten()(x)
+    
+    concatted = Concatenate()([x,input_data_2])
+    x = Dense(units=100, activation='tanh', name='fc')(concatted)
+
+    y_pred = Dense(units=y_sample, activation='softmax', name='softmax')(x) 
+
+    K.clear_session()
+    model = Model(inputs=[input_data_1,input_data_2], outputs=y_pred)
+    model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
+    return model 
+
+def GRU_discriminator(x_sample,y_sample):
+    dtype = 'float64'
+    dropout=0.2
+
+    input_data_1 = Input(name='Main_Features', shape = (x_sample[1],x_sample[2]), dtype=dtype)
+    input_data_2 = Input(name='Silence', shape=(1), dtype=dtype)
+
+    att_in = GRU(32,return_sequences=True,kernel_regularizer=l2())(input_data_1)
+    att_out = GRU(32,return_sequences=False)(att_in)
+    
+    concatted = Concatenate()([att_out,input_data_2])
+    x = Dense(units=128, activation='tanh', name='fc')(concatted)
+    x = Dropout(dropout, name='dropout_2')(x)
+
+    # Output layer with softmax
+    y_pred = Dense(units=y_sample, activation='softmax', name='softmax')(x) 
+
+    K.clear_session()
+    model = Model(inputs=[input_data_1,input_data_2], outputs=y_pred)
+    model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
+    return model 
+
+def MLP_discriminator(x_sample,y_sample):
+    dtype = 'float64'
+    dropout=0.2
+    
+    input_data_1 = Input(name='Main_Features', shape = (x_sample[1],x_sample[2]), dtype=dtype)
+    input_data_2 = Input(name='Silence', shape=(1), dtype=dtype)
+
+    x = Dense(units=128, activation='tanh', name='fc1')(input_data_1)
+    x = Dropout(dropout, name='dropout_2')(x)
+    x = Dense(units=32, activation='tanh', name='fc2')(x)
+    x = Flatten()(x)
+
+    concatted = Concatenate()([x,input_data_2])
+    x = Dense(units=100, activation='tanh', name='fc3')(concatted)
+    x = Dropout(dropout, name='dropout_3')(x)
+
+    # Output layer with softmax
+    y_pred = Dense(units=y_sample, activation='softmax', name='softmax')(x) 
+
+    K.clear_session()
+    model = Model(inputs=[input_data_1,input_data_2], outputs=y_pred)
+    model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
+    return model 
+
+
+def get_model():
+    x_shape = X_train_feat.shape
+    y_shape = Y_test.shape[1]
+    if train_sit == 'LSTM':
+        return LSTM_discriminator(x_shape,y_shape)
+    elif train_sit == 'CNN':
+        return CNN_discriminator(x_shape,y_shape)
+    elif train_sit == 'GRU':
+        return GRU_discriminator(x_shape,y_shape)
+    elif train_sit == 'MLP':
+        return MLP_discriminator(x_shape,y_shape)
+    else:
+        raise Exception("Sorry, invalid Attack Model")
 
 if __name__ == '__main__':
     print("Loading Features.......")
